c4bcac49f71f626df563ae2782fa869d3e7a46f4
sammy-may <samuel.james.may1@gmail.com>
2024-11-12 08:56:40 -0800

    :sparkles: start using tailwind css classes

--- frontend/src/components/RaceCard.tsx
+++ frontend/src/components/RaceCard.tsx
@@ -3,8 +3,8 @@ import RaceType from "../types/race";
 const RaceCard = ({ race }: { race: RaceType }) => {
     return (
         <article className="race_card">
-            <h2>{race.name}</h2>
-            <p>Distance: {race.distance}</p>
+            <h2 className="bg-blue-400">{race.name}</h2>
+            <p className="shadow-teal-500">Distance: {race.distance}</p>
         </article>
     );
 };
