99bb298705eb53a89c1904ac4e21f53dce765145
sammy-may <samuel.james.may1@gmail.com>
2024-12-22 07:49:21 -0800

    :alembic: testing if vercel packages were causing high memory usage

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -4,6 +4,8 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 import { RaceDataProvider } from "../context/RaceFeedContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
@@ -11,6 +13,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <div className="flex-row items-center place-content-between justify-between m-3 max-h-screen min-h-screen">
                 <Header />
                 {children}
+                <Analytics />
+                <SpeedInsights />
                 <Footer />
             </div>
         </RaceDataProvider>
