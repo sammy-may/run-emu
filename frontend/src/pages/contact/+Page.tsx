a527d2cc83424893db15e41d041a41d573ba0bda
sammy-may <samuel.james.may1@gmail.com>
2025-01-08 06:57:13 -0800

    :memo: add contact page

---
+++ frontend/src/pages/contact/+Page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <main className="flex flex-row place-content-evenly items-start px-3">
+            We'd love to hear from you -- reach out to{" "}
+            <address className="font-mono pl-2"> sam@runemu.com</address>.
+        </main>
+    );
+};
+
+export default Page;
